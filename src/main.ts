b15f692cb477760b20936206ce427a06cac25aaa
Maxim Balyaba <mbalyaba@exadel.com>
2021-11-24 17:06:23 +0200

    remove exportVariable

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ export async function run(workspace: string = '.'): Promise<void> {
         const head = core.getInput('head')
 
         // save base and head to env for later steps
-        core.exportVariable('NX_BASE', base || 'HEAD~1')
-        core.exportVariable('NX_HEAD', head || 'HEAD')
+        //core.exportVariable('NX_BASE', base || 'HEAD~1')
+        //core.exportVariable('NX_HEAD', head || 'HEAD')
 
         core.info(`Getting diff from ${base} to ${head || 'HEAD'}...`)
         core.info(`using dir: ${GITHUB_WORKSPACE}`)
@@ -23,7 +23,7 @@ export async function run(workspace: string = '.'): Promise<void> {
         const appsString = JSON.stringify(apps)
 
         core.setOutput('affected_apps', appsString)
-        core.exportVariable('NX_AFFECTED_APPS', appsString)
+        //core.exportVariable('NX_AFFECTED_APPS', appsString)
         core.info(`Found these affected apps: \n ${appsString}`)
     } catch (error) {
         core.setFailed(error.message)
